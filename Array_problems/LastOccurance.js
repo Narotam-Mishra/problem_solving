3e230ac707decf44bff998d0c99ed1e9ae650789
Narotam-Mishra <narotam21jan@gmail.com>
2023-12-09 00:00:23 +0530

    ps_09

---
+++ Array_problems/LastOccurance.js
@@ -0,0 +1,14 @@
+
+function lastOccuranceIndex(arr,x,len){
+    lastIndex = -1;
+    for(let i=0; i<len; i++){
+        if(arr[i] == x){
+            lastIndex = i;
+        }
+    }
+    return lastIndex;
+}
+
+let arr = [4,6,9,7,8,7,3,7,2,];
+let x = 11;
+console.log(lastOccuranceIndex(arr,x,arr.length));
