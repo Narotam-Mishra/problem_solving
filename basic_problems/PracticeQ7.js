f2c15ecdcf30d9e81411d42f37a8f6c450cae056
Narotam-Mishra <narotam21jan@gmail.com>
2023-12-26 23:54:10 +0530

    ps-26

---
+++ basic_problems/PracticeQ7.js
@@ -0,0 +1,11 @@
+
+// Predict Output - 15
+
+const data = [1, "2", 3, "4", 5];
+
+const result = data
+  .map((item) => parseInt(item))
+  .filter((item) => !isNaN(item))
+  .reduce((acc, item) => acc + item, 0);
+
+console.log(result);
