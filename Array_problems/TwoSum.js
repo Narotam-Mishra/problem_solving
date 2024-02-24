f8b9e44bdf73f3d407ffe39d7b7ca9f78c71e31a
Narotam-Mishra <narotam21jan@gmail.com>
2024-02-24 18:04:29 +0530

    ps_24th_Feb_1.2.1

--- Array_problems/TwoSum.js
+++ Array_problems/TwoSum.js
@@ -30,3 +30,40 @@ let twoSum1 = function(nums, target) {
     }
     return res;
 };
+
+// Approach - 3 (Efficient approach using object as map)
+
+let twoSum2 = function (nums, target) {
+  let frqMap = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    if (frqMap[nums[i]]) {
+      // if frequency of number exist then increment it
+      frqMap[nums[i]]++;
+    } else {
+      // add new entry
+      frqMap[nums[i]] = 1;
+    }
+  }
+
+  const res = [];
+  for (let i = 0; i < nums.length; i++) {
+    let b = nums[i];
+    let a = target - b;
+
+    // Check if complement exists in the frequency map
+    // If yes, push the index of the current number and the index of the complement
+    // make sure we avoid dupllicates
+    if (frqMap[a] && (a !== b || frqMap[a] > 1)) {
+      res.push(i);
+      
+      for (let j = i + 1; j < nums.length; j++) {
+        if (nums[j] === a) {
+          res.push(j);
+          return res;
+        }
+      }
+    }
+  }
+  return res;
+};
--- Array_problems/TwoSumII.js
+++ Array_problems/TwoSumII.js
@@ -1,6 +1,7 @@
 
 // Problem link - https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/description/
 
+// Approach - 1 (Efficient approach using Two Pointers)
 let twoSum = function(numbers, target) {
     let s = 0;
     let e = numbers.length-1;
@@ -15,3 +16,42 @@ let twoSum = function(numbers, target) {
         }
     }
 };
+
+// Approach - 2 (Efficient approach using object as Map)
+
+let twoSum2 = function (nums, target) {
+    let frqMap = {};
+  
+    for (let i = 0; i < nums.length; i++) {
+      if (frqMap[nums[i]]) {
+        // if frequency of number exist then increment it
+        frqMap[nums[i]]++;
+      } else {
+        // add new entry
+        frqMap[nums[i]] = 1;
+      }
+    }
+  
+    const res = [];
+    for (let i = 0; i < nums.length; i++) {
+      let b = nums[i];
+      let a = target - b;
+  
+      // Check if complement exists in the frequency map
+      // If yes, push the index of the current number and the index of the complement
+      // make sure we avoid dupllicates
+      if (frqMap[a] && (a !== b || frqMap[a] > 1)) {
+        // 1-based indexing
+        res.push(i+1);
+        
+        for (let j = i + 1; j < nums.length; j++) {
+          if (nums[j] === a) {
+            // 1-based indexing
+            res.push(j+1);
+            return res;
+          }
+        }
+      }
+    }
+    return res;
+  };
