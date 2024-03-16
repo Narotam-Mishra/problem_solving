c3755c8c0c80a861371f38c73ea6103c156247b8
Narotam-Mishra <narotam21jan@gmail.com>
2024-03-16 05:55:47 +0530

    ps_16th_Mar_1.2

--- binarySearch_problems/SingleElement.js
+++ binarySearch_problems/SingleElement.js
@@ -56,3 +56,13 @@ let singleNonDuplicate1 = function(arr){
         }
     }
 }
+
+// Approach - 3 (using XOR)
+
+let singleNonDuplicate2 = function(nums) {
+    let ans = 0;
+    for(let i=0; i<nums.length; i++){
+        ans = ans ^ nums[i];
+    }
+    return ans;
+};
