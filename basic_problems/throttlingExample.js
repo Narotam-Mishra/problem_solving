e7890f22c1072b889f35af853e37027ad245fefd
Narotam-Mishra <narotam21jan@gmail.com>
2024-04-14 00:06:54 +0530

    ps_14th_Apr_1.1

--- basic_problems/throttlingExample.js
+++ basic_problems/throttlingExample.js
@@ -1,3 +1,31 @@
 
 // Throttling in JS
 
+function throttle(func, delay){
+    let lastCall = 0;
+
+    return function(...args){
+        const now = new Date().getTime();
+
+        if(now - lastCall < delay){
+            // not enough time has passed since the last call, do nothing
+            return;
+        }
+
+        // enough time has passed, call the function the need to be throttled
+        lastCall = now;
+        func(...args);
+    };
+}
+
+// example usage:
+
+// function to be trottled
+function handleInput(event){
+    console.log("Input Received:", event.target.value);
+}
+// Throttle the input handling function to only run once every 1000 milliseconds
+const throttledHandleInput = throttle(handleInput, 1000);
+
+// Attach the throttle function to an input element
+document.getElementById('myInput').addEventListener('input', throttledHandleInput);
