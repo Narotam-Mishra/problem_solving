8d605fdaf80b42270be5af8969d640083282ed06
Narotam-Mishra <narotam21jan@gmail.com>
2024-04-28 10:10:23 +0530

    ps_28th_Apr_1.1

---
+++ Stack_Problems/MaxFrequencyStack.js
@@ -0,0 +1,21 @@
+
+// Problem link - https://leetcode.com/problems/maximum-frequency-stack/description/
+
+var FreqStack = function() {
+    
+};
+
+/** 
+ * @param {number} val
+ * @return {void}
+ */
+FreqStack.prototype.push = function(val) {
+    
+};
+
+/**
+ * @return {number}
+ */
+FreqStack.prototype.pop = function() {
+    
+};
