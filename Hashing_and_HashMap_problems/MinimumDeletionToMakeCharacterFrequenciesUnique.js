0260f9981ed19ffaa3cd3f9b1a082d82fd33463b
Narotam-Mishra <narotam21jan@gmail.com>
2025-05-24 13:39:31 +0530

    ps_24th_May_Prb-1647_1.2

--- Hashing_and_HashMap_problems/MinimumDeletionToMakeCharacterFrequenciesUnique.js
+++ Hashing_and_HashMap_problems/MinimumDeletionToMakeCharacterFrequenciesUnique.js
@@ -1,7 +1,8 @@
 
 // Problem link - https://leetcode.com/problems/minimum-deletions-to-make-character-frequencies-unique/description/
 
-let minDeletions = function(s) {
+// Appraoch - 1 (using object as Hash Map)
+let minDeletions1 = function(s) {
     // step 1 - store frequency of each character 
     const frqMap = {};
     for(let ch of s){
@@ -36,9 +37,42 @@ let minDeletions = function(s) {
     return deletionCount;
 };
 
-// const s = "aab";
+// Appraoch - 12 (using fixed size array)
+let minDeletions = function(s) {
+    // step 1 - store frequency of each character 
+    const frqMap = new Array(26).fill(0);
+    for(let ch of s){
+        frqMap[ch.charCodeAt(0) - 'a'.charCodeAt(0)]++;
+    }
+
+    // step 2 - use set to store used frequencies to ensure uniqueness
+    const usedFrequencies = new Set();
+    let deletionCount = 0;
+
+    // step 3 - iterate on each frquency values
+    for(let frq of frqMap){
+        // step 4 - reduce frequency until it's unique (or zero)
+        while(frq > 0 && usedFrequencies.has(frq)){
+            // simulate deleting one occurrence
+            frq--;
+
+            // count minimum deletion required
+            deletionCount++;
+        }
+
+        // step 5 - if `frq > 0`, add it to the set of used frquencies
+        if(frq > 0){
+            usedFrequencies.add(frq);
+        }
+    }
+
+    // step 6 - return minimum deletion needed to make good string
+    return deletionCount;
+};
+
+const s = "aab";
 
 // const s = "aaabbbcc";
 
-const s = "ceabaacb";
+// const s = "ceabaacb";
 console.log(minDeletions(s));
