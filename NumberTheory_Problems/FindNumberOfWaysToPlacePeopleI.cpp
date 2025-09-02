1f89e4e5b763325258b6261d6864941ae58c3289
Narotam-Mishra <narotam21jan@gmail.com>
2025-09-02 18:54:44 +0530

    ps_2nd_Sept_Prb-3025_1.2

--- NumberTheory_Problems/FindNumberOfWaysToPlacePeopleI.cpp
+++ NumberTheory_Problems/FindNumberOfWaysToPlacePeopleI.cpp
@@ -7,55 +7,55 @@ using namespace std;
 int numberOfPairs(vector<vector<int>>& points) {
     int n = points.size();
 
-        // store count of valid points
-        int count = 0;
+    // store count of valid points
+    int count = 0;
 
-        // step 1 - try every pair of points
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                // skip same points
-                if (i == j)
-                    continue;
+    // step 1 - try every pair of points
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            // skip same points
+            if (i == j)
+                continue;
 
-                // extract co-ordinate points
-                vector<int>& A = points[i];
-                vector<int>& B = points[j];
+            // extract co-ordinate points
+            vector<int>& A = points[i];
+            vector<int>& B = points[j];
 
-                // step 2 - check first condition : if A is on upper left of B
-                // A is on upper left if A.x <= B.x and A.y >= b.y
-                if (A[0] <= B[0] && A[1] >= B[1]) {
-                    // step 3 - check second condition : there is no points inside rectangle
-                    bool hasPointsInside = false;
+            // step 2 - check first condition : if A is on upper left of B
+            // A is on upper left if A.x <= B.x and A.y >= b.y
+            if (A[0] <= B[0] && A[1] >= B[1]) {
+                // step 3 - check second condition : there is no points inside rectangle
+                bool hasPointsInside = false;
 
-                    // check every other possible points to see if it lies in the rectangle
-                    for (int k = 0; k < n; k++) {
-                        // skip same points
-                        if (k == i || k == j)
-                            continue;
+                // check every other possible points to see if it lies in the rectangle
+                for (int k = 0; k < n; k++) {
+                    // skip same points
+                    if (k == i || k == j)
+                        continue;
 
-                        // extract co-ordinate points
-                        vector<int>& point = points[k];
-                        int x = point[0];
-                        int y = point[1];
+                    // extract co-ordinate points
+                    vector<int>& point = points[k];
+                    int x = point[0];
+                    int y = point[1];
 
-                        // check if this point lies inside rectangle or not
-                        if (x >= A[0] && x <= B[0] && y >= B[1] && y <= A[1]) {
-                            hasPointsInside = true;
-                            break;
-                        }
+                    // check if this point lies inside rectangle or not
+                    if (x >= A[0] && x <= B[0] && y >= B[1] && y <= A[1]) {
+                        hasPointsInside = true;
+                        break;
                     }
+                }
 
-                    // step 4 - if no points lies inside rectangle
-                    if (!hasPointsInside) {
-                        // this is valid pair, so increment count
-                        count++;
-                    }
+                // step 4 - if no points lies inside rectangle
+                if (!hasPointsInside) {
+                    // this is valid pair, so increment count
+                    count++;
                 }
             }
         }
+    }
 
-        // step 5 - return valid count
-        return count;
+    // step 5 - return valid count
+    return count;
 }
 
 int main(){
