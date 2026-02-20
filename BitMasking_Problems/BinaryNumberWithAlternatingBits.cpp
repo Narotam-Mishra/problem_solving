3987fd93f1351313ffc2a6b2194c5168c188dfa9
Narotam-Mishra <narotam21jan@gmail.com>
2026-02-20 22:59:32 +0530

    ps_19th_Feb_added

--- BitMasking_Problems/BinaryNumberWithAlternatingBits.cpp
+++ BitMasking_Problems/BinaryNumberWithAlternatingBits.cpp
@@ -4,7 +4,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-bool hasAlternatingBits(int n) {
+bool hasAlternatingBits1(int n) {
     // XOR the number with its right-shifted version by 1 bit.
     // this helps to check if adjacent bits are different.
     unsigned int x = (n >> 1) ^ n;
@@ -16,10 +16,34 @@ bool hasAlternatingBits(int n) {
     return ans;
 }
 
+bool hasAlternatingBits(int n) {
+    // using bitset container to store number in bits
+    bitset<32> b(n);
+
+    // step 1 - find highest set bit
+    int highestSetBit = log2(n);
+
+    // step 2 - iterate on each bit 
+    for(int i=0; i<=highestSetBit; i++){
+        // step 3 - check if it has consecutive bit or not
+        if(b[i] == b[i+1]){
+            // return false immediately
+            return false;
+        }
+    }
+
+    // step 4 - return true once all bit iteration completed
+    return true;
+}
+
 int main(){
     // int num = 5;
 
-    int num = 9;
+    // int num = 9;
+
+    // int num = 7;
+
+    int num = 11;
     bool res = hasAlternatingBits(num);
     res == 1 ? cout<<"true" : cout<<"false";
     return 0;
--- string_problems/CountBinarySubstrings.cpp
+++ string_problems/CountBinarySubstrings.cpp
@@ -1,8 +1,10 @@
 
+// Problem link - https://leetcode.com/problems/count-binary-substrings/description/?envType=daily-question&envId=2026-02-19
+
 #include<bits/stdc++.h>
 using namespace std;
 
-int countBinarySubstrings(string s) {
+int countBinarySubstrings1(string s) {
     // step 1 - variable intialization
     int prevGroup = 0, currGroup = 1, count = 0;
 
@@ -25,6 +27,34 @@ int countBinarySubstrings(string s) {
 
     // step 3 - count last valid substring
     return count + min(currGroup, prevGroup);
+}
+
+int countBinarySubstrings(string s) {
+    int n = s.length();
+
+    // keep track of ccurrent and previous consective streak count
+    int currCnt=1, prevCnt = 0;
+
+    // step 1 - iterate on each character of string
+    int res = 0;
+    for(int i=1; i<n; i++){
+        // step 2 - extend current group if current and previous digits are equal
+        if(s[i] == s[i-1]){
+            currCnt++;
+        }else{
+            // step 3 - otherwise count valid substring
+            res += min(currCnt, prevCnt);
+
+            // step 4 - move to new group
+            prevCnt = currCnt;
+
+            // step 5 - reset counter for the new group
+            currCnt = 1;
+        }
+    }
+
+    // step 6 - count last valid substring and return result, `res`
+    return res + min(currCnt, prevCnt);
 }
 
 int main(){
