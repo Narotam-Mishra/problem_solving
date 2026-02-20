252da5cc0d49416542fc5b2a36266aa59d17bf2c
Narotam-Mishra <narotam21jan@gmail.com>
2026-02-20 23:27:44 +0530

    ps_20th_Feb_Prb-761_1.1

---
+++ Recursion_problems/SpecialBinaryString.cpp
@@ -0,0 +1,15 @@
+
+// Problem link - https://leetcode.com/problems/special-binary-string/description/?envType=daily-question&envId=2026-02-20
+
+#include<bits/stdc++.h>
+using namespace std;
+
+string makeLargestSpecial(string s) {
+        
+}
+
+int main(){
+    string s = "11011000";
+    cout<<makeLargestSpecial(s);
+    return 0;
+}
