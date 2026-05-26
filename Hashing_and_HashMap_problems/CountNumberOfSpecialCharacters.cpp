02fa0c4d0ac85d59121110fb3aba99180b8b7039
Narotam-Mishra <narotam21jan@gmail.com>
2026-05-26 07:38:18 +0530

    ps_26th_May_prb-3120_1.2

--- Hashing_and_HashMap_problems/CountNumberOfSpecialCharacters.cpp
+++ Hashing_and_HashMap_problems/CountNumberOfSpecialCharacters.cpp
@@ -2,9 +2,9 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int numberOfSpecialChars(string word) {
+int numberOfSpecialChars1(string word) {
     // step 1 - initialize two sets to store lowercase and uppercase letters
-        // to store all uppercase letters
+    // to store all uppercase letters
     unordered_set<char> upperSet;
 
     // to store all lowercase letters
@@ -34,12 +34,42 @@ int numberOfSpecialChars(string word) {
     return specialCharCount;
 }
 
+int numberOfSpecialChars(string word) {
+    // take fixed size array of 52 to track existence of characters
+    int frq[52] = {0};
+    
+    // step 1 - mark existence of each character in the word
+    for(char ch : word){
+        if(islower(ch)){
+            frq[ch - 'a'] = 1;
+        }else{
+            frq[ch - 'A' + 26] = 1;
+        }
+    }
+
+    // keep track of special count
+    int cnt = 0;
+
+    // step 2 - check each letter (0-25) if both its lowercase 
+    // and uppercase version exist or not
+    for(int i=0; i<26; i++){
+        // check if both frequencies must be 1 for the letter to be "special"
+        if(frq[i] == 1 && frq[i+26] == 1){
+            // increment count
+            cnt++;
+        }
+    }
+
+    // step 3 - return total count of special letters
+    return cnt;
+}
+
 int main(){
-    // string word = "aaAbcBC";
+    string word = "aaAbcBC";
 
     // string word = "abc";
 
-    string word = "abBCab";
+    // string word = "abBCab";
     cout<<numberOfSpecialChars(word)<<endl;
     return 0;
 }
--- Hashing_and_HashMap_problems/CountNumberOfSpecialCharacters.java
+++ Hashing_and_HashMap_problems/CountNumberOfSpecialCharacters.java
@@ -6,7 +6,7 @@ package Hashing_and_HashMap_problems;
 import java.util.*;
 
 public class CountNumberOfSpecialCharacters {
-    public static int numberOfSpecialChars(String word) {
+    public static int numberOfSpecialChars1(String word) {
         // step 1 - initialize two sets to store lowercase and uppercase letters
         // to store all uppercase letters
         Set<Character> upperSet = new HashSet<>();
@@ -35,6 +35,36 @@ public class CountNumberOfSpecialCharacters {
 
         // step 4 - return the total count of special letters
         return specialCharCount;
+    }
+
+    public static  int numberOfSpecialChars(String word) {
+        // take fixed size array of 52 to track existence of characters
+        int frq[] = new int[52];
+
+        // step 1 - mark existence of each character in the word
+        for (char ch : word.toCharArray()) {
+            if (Character.isLowerCase(ch)) {
+                frq[ch - 'a'] = 1;
+            } else {
+                frq[ch - 'A' + 26] = 1;
+            }
+        }
+
+        // keep track of special count
+        int cnt = 0;
+
+        // step 2 - check each letter (0-25) if both its lowercase
+        // and uppercase version exist or not
+        for (int i = 0; i < 26; i++) {
+            // check if both frequencies must be 1 for the letter to be "special"
+            if (frq[i] == 1 && frq[i + 26] == 1) {
+                // increment count
+                cnt++;
+            }
+        }
+
+        // step 3 - return total count of special letters
+        return cnt;
     }
 
     public static void main(String[] args) {
